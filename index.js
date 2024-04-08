dbd7f2b4808098c6dcb51a5ed124f8b09a310b21
Mark Mwiti <muthamiamark@gmail.com>
2024-04-08 13:44:53 +0300

    Adds features to index.j

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+   //Access the global variable customerName
+   return customerName.toUpperCase();
+} 
+
+//Call the funtion and store the uppercase version in a new variable
+var uppercaseName = upperCaseCustomerName();
+console.log(uppercaseName); //Output: BOB
